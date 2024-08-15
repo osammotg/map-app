93533c8efb4174b45a4dc45065cb38b3fc237497
osammotg <63055133+osammotg@users.noreply.github.com>
2024-08-15 23:20:29 +0200

    map in black and white

--- src/MyMap copy.js
+++ src/MyMap copy.js
@@ -19,10 +19,9 @@ function MyMap() {
       zoom={13}
       style={{ height: "100vh", width: "100%" }}
     >
-      {/* Black and White Tile Layer */}
       <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
 
       {/* First Marker */}
